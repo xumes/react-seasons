eede67e44017c13fdcb0ccadff56bc0671050887
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-04-22 23:05:06 -0300

    capturing user location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position.coords.latitude, position.coords.longitude),
+    err => console.log(err)
+  );
   return <div>Hi</div>
 }
 
