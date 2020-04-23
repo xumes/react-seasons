5dc38a4d6a889872736b5171e46feebe456bed3e
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-04-23 17:11:28 -0300

    capturing users location and displaying message accordingly

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from 'react'
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>
+const seasonConfig = {
+  summer: {
+    text: 'Bora pra praia?',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Ai que frio!',
+    iconName: 'snowflake'
+  }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9){
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const {text, iconName} = seasonConfig[season]
+
+  return (
+    <div>
+      <i className={`${iconName} massive icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} massive icon`} />
+    </div>
+  )
 }
 
 export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position.coords.latitude, position.coords.longitude),
-    err => console.log(err)
-  );
-  return <div>Hi</div>
+import SeasonDisplay from './SeasonDisplay'
+
+class App extends React.Component {
+
+  state = {
+    latitude: null,
+    longitude: null,
+    hasError: false,
+    err: false
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      err => {
+        console.log(err);
+        this.setState({
+          hasError: true,
+          err: err
+        })
+      }
+    );
+  }
+
+  render(){
+      if (this.state.hasError && !this.state.latitude) {
+        return (
+          <div>
+            <p>Ops</p>
+            <p>Err: {this.state.err.message}</p>
+          </div>
+        )
+      }
+
+      if (!this.state.hasError && this.state.latitude) {
+        return <SeasonDisplay lat={this.state.latitude} long={this.state.longitude} />
+      }
+
+      return <div>Loading!</div>
+  }
 }
 
 ReactDOM.render(
